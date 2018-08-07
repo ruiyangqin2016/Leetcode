b32bd9bb51b66f756da3ea002158a430cdd70ada
ruiyangqin2016 <ruiyangqin2016@gmail.com>
2018-08-07 12:55:30 -0400

    first commit

---
+++ src/Q14_LCP/Solution.java
@@ -0,0 +1,17 @@
+package Q14_LCP;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+
+        String ret = "";
+        for (int i = 0; i < strs.length; i++) {
+
+        }
+
+        return null;
+    }
+}
