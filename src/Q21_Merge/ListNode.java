221dfdc32a89d257f57373fef0b41218818306fe
ruiyangqin2016 <ruiyangqin2016@gmail.com>
2018-08-07 20:42:40 -0400

    test

---
+++ src/Q21_Merge/ListNode.java
@@ -0,0 +1,9 @@
+package Q21_Merge;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/Q21_Merge/Solution.java
@@ -0,0 +1,14 @@
+package Q21_Merge;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode ret = new ListNode(0);
+
+
+        return ret;
+    }
+}
